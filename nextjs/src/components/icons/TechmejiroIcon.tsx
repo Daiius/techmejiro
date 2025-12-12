e0e72b1425308cafa9cc2f5508b6935d0a2de27d
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-12-12 21:49:49 +0900

    update techmejiro icon and components

---
+++ nextjs/src/components/icons/TechmejiroIcon.tsx
@@ -0,0 +1,165 @@
+import type { SVGProps } from "react";
+export const TechmejiroIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width="1em"
+    height="1em"
+    viewBox="0 0 100 100"
+    {...props}
+  >
+    <defs>
+      <radialGradient
+        xlinkHref="#techmejiro_svg__a"
+        id="techmejiro_svg__b"
+        cx={52.146}
+        cy={88.143}
+        r={38.622}
+        fx={52.146}
+        fy={88.143}
+        gradientTransform="matrix(2.84814 0 0 1.73479 -93.518 -62.91)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="techmejiro_svg__a">
+        <stop
+          offset={0.306}
+          style={{
+            stopColor: "#fff0d2",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={0.5}
+          style={{
+            stopColor: "#d9bd3e",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={0.751}
+          style={{
+            stopColor: "#00da21",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+    </defs>
+    <path
+      d="M45 15c8.971.264 19.098 3.238 25 10 9.038 10.354 12.023 26.406 10 40-1.43 9.612-6.543 20.212-15 25-13.053 7.39-31.947 7.39-45 0-8.457-4.788-13.57-15.388-15-25-2.023-13.594.662-29.916 10-40 7.162-7.734 19.464-10.31 30-10"
+      style={{
+        fill: "url(#techmejiro_svg__b)",
+        fillOpacity: 1,
+        stroke: "#262626",
+        strokeWidth: 1,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeDasharray: "none",
+        paintOrder: "markers stroke fill",
+        fillRule: "evenodd",
+      }}
+    />
+    <ellipse
+      cx={35.922}
+      cy={33}
+      rx={9.875}
+      ry={10}
+      style={{
+        fill: "#f5fff7",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 1,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeDasharray: "none",
+        paintOrder: "markers stroke fill",
+      }}
+    />
+    <circle
+      cx={36.125}
+      cy={33}
+      r={7}
+      style={{
+        fill: "#001203",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 0.999997,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeDasharray: "none",
+        paintOrder: "markers stroke fill",
+      }}
+    />
+    <ellipse
+      cx={60.125}
+      cy={33}
+      rx={9.875}
+      ry={10}
+      style={{
+        fill: "#f5fff7",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 1,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeDasharray: "none",
+        paintOrder: "markers stroke fill",
+      }}
+    />
+    <circle
+      cx={60}
+      cy={33}
+      r={7}
+      style={{
+        fill: "#001203",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 0.999997,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeDasharray: "none",
+        paintOrder: "markers stroke fill",
+      }}
+    />
+    <path
+      d="M50 45c7.152-5 35-2 35-2s-22.215 7-30 7c-2.357 0-6.932-3.65-5-5"
+      style={{
+        fill: "#001203",
+        fillOpacity: 1,
+        stroke: "#262626",
+        strokeWidth: 1,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeDasharray: "none",
+        paintOrder: "markers stroke fill",
+      }}
+    />
+    <path
+      d="M25 51c4.43 7.382-5.625 35.646-15 28.939C.512 73.15 4.734 49.634 4.734 49.634L15 46s5.498-2.504 10 5"
+      style={{
+        fill: "#00da21",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 1,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeDasharray: "none",
+        strokeOpacity: 1,
+        paintOrder: "markers stroke fill",
+      }}
+    />
+    <path
+      d="M25 51c4.43 7.382-5.625 35.646-15 28.939C.512 73.15 4.514 49.413 4.514 49.413"
+      style={{
+        fill: "none",
+        fillOpacity: 1,
+        stroke: "#262626",
+        strokeWidth: 0.5,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeDasharray: "none",
+        strokeOpacity: 1,
+        paintOrder: "markers stroke fill",
+      }}
+    />
+  </svg>
+);
